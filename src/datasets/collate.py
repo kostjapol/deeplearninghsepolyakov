1fdd76a27ee93b3bf98d1da17a0f6af296825578
Konstantin Polyakov <konstantinpolyakov@MacBook-Air-Konstantin.local>
2026-07-30 20:14:26 +0300

    Pad variable-length audio batches

--- src/datasets/collate.py
+++ src/datasets/collate.py
@@ -1,25 +1,16 @@
 import torch
+from torch.nn.utils.rnn import pad_sequence
 
 
 def collate_fn(dataset_items: list[dict]):
-    """
-    Collate and pad fields in the dataset items.
-    Converts individual items into a batch.
-
-    Args:
-        dataset_items (list[dict]): list of objects from
-            dataset.__getitem__.
-    Returns:
-        result_batch (dict[Tensor]): dict, containing batch-version
-            of the tensors.
-    """
-
     result_batch = {}
 
-    # example of collate_fn
-    result_batch["data_object"] = torch.vstack(
-        [elem["data_object"] for elem in dataset_items]
+    result_batch["data_object"] = pad_sequence(
+        [elem["data_object"] for elem in dataset_items],
+        batch_first=True,
+        padding_value=0,
     )
+
     result_batch["labels"] = torch.tensor([elem["labels"] for elem in dataset_items])
 
     return result_batch
